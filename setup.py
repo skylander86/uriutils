b794dac92485623ebd7b21726841fb75d064427c
yc <yanchuan@outlook.com>
2017-07-16 22:22:44 +0800

    Bump version for pypi

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='uriutils',
-    version='0.1',
+    version='0.1.1',
     description='Easily read and writ to different storage platforms in Python.',
     long_description='Working with multiple storage platforms (i.e., local filesystem, S3, Google Cloud, etc.) can be quite a hassle. This package aims to make it transparent to the user and the developer the underlying storage system by wrapping the different protocols in a common interface.',
     classifiers=[
